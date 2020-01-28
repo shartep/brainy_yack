e3b9d62ab79148e6f3e53d4bd5a9769cef9d30a3
shartep <shartep0@gmail.com>
2020-01-28 16:27:03 +0200

    remove rails classic controllers and unused views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,10 @@
 # base class for all controllers, helpful for different helper methods related to params, current_user etc.
 class ApplicationController < ActionController::Base
-  private
-
-  # As I delegate a lot of validation and param conversion to services, I don't need most
-  # of strong params functionality. So, this method just produces the regular Ruby Hash with
-  # Symbol keys, listed as arguments -- ready to passed as keyword args.
-  def param_hash(*keys)
-    params.to_unsafe_h
-          .deep_symbolize_keys
-          .then { |hash| keys.empty? ? hash : hash.slice(*keys) }
-          .compact
+  def clear_cache
+    Rails.cache.clear
   end
 
-  def cache_key(*keys)
-    param_hash(keys).then do |params|
-      keys.map { |k| "#{k}--#{params[k]}" }.join('/')
-    end
+  def notify_active_users
+    # implement notification via ActiveCable here
   end
 end
